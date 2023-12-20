7db57e73f4061b97ae96b21de34e756c5a9d83eb
James Snell <vulpes3.1415@yahoo.com>
2023-12-19 18:52:00 -0700

    Completed tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  function addTask(event) {
+    console.log('Preventing refresh');
+    event.preventDefault();
+
+    const taskInput = document.getElementById("new-task-description");
+    const taskList = document.getElementById("tasks");
+    console.log(taskInput)
+
+    // document.querySelector('#text').textContent = "new-task-description"
+
+    const task = document.createElement("li");
+    task.textContent = taskInput.value;
+    taskList.appendChild(task);
+
+    taskInput.value = ""; // Clear the input field
+  }
+
+  const form = document.getElementById("create-task-form");
+  console.log(form)
+  form.addEventListener("submit", addTask);
 });
+
+
